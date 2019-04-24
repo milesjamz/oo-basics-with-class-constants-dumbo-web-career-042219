861931ca8264d849891ed7969226f04c271a729b
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 15:42:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if brand != BRANDS[2]
+    BRANDS << brand
+    end
   end
 
   def cobble
